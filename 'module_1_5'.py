42ea110ddd497dbea0edf8c59e89b6da4c7da9db
ANDREY1483 <moshechkov83@mail.ru>
2024-08-08 02:00:39 +0300

    Add files via upload

---
+++ 'module_1_5'.py
@@ -0,0 +1,9 @@
+immutable_var = 1, 2, 3, "apple", True
+print('Immutable tuple',":", immutable_var)
+# immutable_var [2] = 'book'
+# print('Immutable tuple',":", immutable_var)
+# Это такой вид команды для сохранения не изменяемости данных
+mutable_list = [1, 2, 3, 'Apple', False ]
+print('Mutable list',":",mutable_list)
+mutable_list [2]= 'book'
+print('Mutable list',":",mutable_list)
